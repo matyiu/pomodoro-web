87a79982bcf683da049f2834aca1b1db4f7f188a
Jefferson Valle <valleespinozajefferson@gmail.com>
2019-03-19 14:05:54 +0100

    Live Reload

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'dist')
+    contentBase: path.join(__dirname, 'dist'),
+    publicPath: '/js/'
   }
 })
